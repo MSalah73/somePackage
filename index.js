0c046109280de2cedc940acadefd091984a79d85
MSalah73 <ms24@pdx.edu>
2019-10-23 12:41:21 +0300

    annyoing package

---
+++ index.js
@@ -0,0 +1,8 @@
+
+let Mo = "I hate kubernetes its so tedious!!!!";
+let Midhad ="KAUST is life"
+let Nujud = "I like Greece"
+let Wajdan  = "I love today's coffee"
+
+
+module.exports = {Mo, Midhad, Nujud, Wajdan}
